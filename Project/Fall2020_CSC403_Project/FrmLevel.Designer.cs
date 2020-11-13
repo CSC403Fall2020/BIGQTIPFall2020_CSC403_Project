377329a2cf3b6b889fde21291c61487c2d010226
Landry Baudouin <landryb02@gmail.com>
2020-11-13 11:34:06 -0600

    Fixed tabbage issues that were creating build errors

--- Project/Fall2020_CSC403_Project/FrmLevel.Designer.cs
+++ Project/Fall2020_CSC403_Project/FrmLevel.Designer.cs
@@ -2,31 +2,36 @@
 using System.Runtime.CompilerServices;
 using System.Windows.Forms;
 
-namespace Fall2020_CSC403_Project {
-  partial class FrmLevel {
-    /// <summary>
-    /// Required designer variable.
-    /// </summary>
-    private System.ComponentModel.IContainer components = null;
+namespace Fall2020_CSC403_Project
+{
+	partial class FrmLevel
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
 
-    /// <summary>
-    /// Clean up any resources being used.
-    /// </summary>
-    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-    protected override void Dispose(bool disposing) {
-      if (disposing && (components != null)) {
-        components.Dispose();
-      }
-      base.Dispose(disposing);
-    }
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
 
-    #region Windows Form Designer generated code
+		#region Windows Form Designer generated code
 
-    /// <summary>
-    /// Required method for Designer support - do not modify
-    /// the contents of this method with the code editor.
-    /// </summary>
-    private void InitializeComponent() {
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
 			this.components = new System.ComponentModel.Container();
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmLevel));
 			this.tmrUpdateInGameTime = new System.Windows.Forms.Timer(this.components);
@@ -381,36 +386,35 @@ namespace Fall2020_CSC403_Project {
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
-    }
+		}
 
-    #endregion
+		#endregion
 
-    private System.Windows.Forms.PictureBox picPlayer;
-    private System.Windows.Forms.PictureBox picBossKoolAid;
-    private System.Windows.Forms.Timer tmrUpdateInGameTime;
-    private System.Windows.Forms.Timer tmrPlayerMove;
-    private System.Windows.Forms.PictureBox picWall3;
-    private System.Windows.Forms.PictureBox picEnemyPoisonPacket;
-    private System.Windows.Forms.PictureBox picEnemyCheeto;
-    private System.Windows.Forms.PictureBox picWall5;
-    private System.Windows.Forms.PictureBox picWall4;
-    private System.Windows.Forms.PictureBox picWall12;
-    private System.Windows.Forms.PictureBox picWall6;
-    private System.Windows.Forms.PictureBox picWall9;
-    private System.Windows.Forms.PictureBox picWall10;
-    private System.Windows.Forms.PictureBox picWall0;
-    private System.Windows.Forms.PictureBox picWall7;
-    private System.Windows.Forms.PictureBox picWall8;
-    private System.Windows.Forms.PictureBox picWall1;
-    private System.Windows.Forms.PictureBox picWall2;
-    private System.Windows.Forms.PictureBox picWall11;
+		private System.Windows.Forms.PictureBox picPlayer;
+		private System.Windows.Forms.PictureBox picBossKoolAid;
+		private System.Windows.Forms.Timer tmrUpdateInGameTime;
+		private System.Windows.Forms.Timer tmrPlayerMove;
+		private System.Windows.Forms.PictureBox picWall3;
+		private System.Windows.Forms.PictureBox picEnemyPoisonPacket;
+		private System.Windows.Forms.PictureBox picEnemyCheeto;
+		private System.Windows.Forms.PictureBox picWall5;
+		private System.Windows.Forms.PictureBox picWall4;
+		private System.Windows.Forms.PictureBox picWall12;
+		private System.Windows.Forms.PictureBox picWall6;
+		private System.Windows.Forms.PictureBox picWall9;
+		private System.Windows.Forms.PictureBox picWall10;
+		private System.Windows.Forms.PictureBox picWall0;
+		private System.Windows.Forms.PictureBox picWall7;
+		private System.Windows.Forms.PictureBox picWall8;
+		private System.Windows.Forms.PictureBox picWall1;
+		private System.Windows.Forms.PictureBox picWall2;
+		private System.Windows.Forms.PictureBox picWall11;
 		private System.Windows.Forms.PictureBox picEnemyTony;
 		private System.Windows.Forms.PictureBox picEnemyRonald;
 		private System.Windows.Forms.Label lblInGameTime;
-	}
+		//this exits the entire game
+		private System.Windows.Forms.Label btnExitGame;
 
-    //this exits the entire game
-    private System.Windows.Forms.Label btnExitGame;
-    }
+	}
 }
 
